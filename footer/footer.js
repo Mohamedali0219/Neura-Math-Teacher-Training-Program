d77a3264b9864b922654e4be58d3d226a38efb3a
mohamedali0219 <mohamedali.d2002@gmail.com>
2026-05-24 22:39:48 +0300

    adding the contact info detials

--- footer/footer.js
+++ footer/footer.js
@@ -25,8 +25,8 @@ document.getElementById('section-footer').innerHTML = `
         <h4 class="lang-en">Contact</h4>
         <h4 class="lang-ar">تواصل معنا</h4>
         <ul>
-          <li><a href="mailto:info@neura.com"><i class="fas fa-envelope"></i> info@neura.com</a></li>
-          <li><a href="tel:+1234567890"><i class="fas fa-phone"></i> +1 234 567 890</a></li>
+          <li><a href="https://wa.me/201090023648"><i class="fab fa-whatsapp"></i> 01090023648</a></li>
+          <li><a href="tel:+201060019646"><i class="fas fa-phone"></i> 01060019646</a></li>
           <li><a href="#"><i class="fas fa-map-marker-alt"></i> <span class="lang-en">Online &mdash; Worldwide</span><span class="lang-ar">أونلاين &mdash; حول العالم</span></a></li>
         </ul>
       </div>
@@ -34,16 +34,15 @@ document.getElementById('section-footer').innerHTML = `
         <h4 class="lang-en">Follow Us</h4>
         <h4 class="lang-ar">تابعنا</h4>
         <div class="footer-social">
-          <a href="#" class="social-link" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-          <a href="#" class="social-link" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-          <a href="#" class="social-link" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
-          <a href="#" class="social-link" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-          <a href="#" class="social-link" aria-label="YouTube"><i class="fab fa-youtube"></i></a>
+          <a href="https://youtube.com/@neura-learn?si=yfQF8lwV6-2SVnd9" class="social-link" aria-label="YouTube"><i class="fab fa-youtube"></i></a>
+          <a href="https://www.facebook.com/share/1D8f1extnW/?mibextid=wwXIfr" class="social-link" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/neura.learn?igsh=MXQ3cTN2OHg3MzR3NA%3D%3D&utm_source=qr" class="social-link" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
         </div>
       </div>
     </div>
     <div class="footer-bottom">
-      <p><span class="lang-en">Made BY Yz technology solutions</span><span class="lang-ar">من تصميم Yz technology solutions</span></p>
+      <p><span class="lang-en">Made BY <a href="https://yzsolutions-eg.com/" target="_blank" class="footer-brand-link"><img src="images/Yz_technology solutions.jpg" alt="YZ" class="footer-brand-img"><span>Yz_technology solutions</span></a></span><span class="lang-ar">من تصميم <a href="https://yzsolutions-eg.com/" target="_blank" class="footer-brand-link"><img src="images/Yz_technology solutions.jpg" alt="YZ" class="footer-brand-img"><span>Yz_technology solutions</span></a></span></p>
+      <p class="footer-copyright">&copy; 2026 <span class="lang-en">All rights reserved.</span><span class="lang-ar">جميع الحقوق محفوظة.</span></p>
     </div>
   </div>
 </footer>`;
